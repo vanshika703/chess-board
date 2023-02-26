50ad2e1c1f6258cde2d02a1fef4a496b02e58c96
Vanshika <vanshikabhojnagarwala@gmail.com>
2023-02-26 17:31:34 +0530

    chess app created for roc8 assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [highlight, setHighlight] = useState([]);
+  const [current, setCurrent] = useState([]);
+  function handleOnClick(i, j) {
+    let data = [];
+    const ones = [-2, +2];
+    const twos = [-1, +1];
+    ones.forEach((one) => {
+      return twos.forEach((two) => {
+        data.push([i + one, j + two]);
+        data.push([i + two, j + one]);
+      });
+    });
+    const val = data.filter((value) => value[0] >= 0 && value[1] >= 0);
+    console.log(val);
+    setHighlight(data);
+    setCurrent([i, j]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex-center">
+        {[...Array(8)].map((_e, i) => {
+          return [...Array(8)].map((_e, j) => {
+            const isAlt =
+              (i % 2 === 0 && j % 2 !== 0) || (i % 2 !== 0 && j % 2 === 0);
+            const isHigh = highlight.find((h) => h[0] === i && h[1] === j);
+            const isCurrent = current[0] === i && current[1] === j;
+            return (
+              <div
+                className={
+                  "box " +
+                  (isAlt ? "alt " : "") +
+                  (isHigh ? "highlight" : "") +
+                  (isCurrent ? "current" : "")
+                }
+                key={`${i + " " + j}`}
+                onClick={() => handleOnClick(i, j)}
+              >
+                {/* {i + " ," + j}{" "} */}
+              </div>
+            );
+          });
+        })}
+      </div>
     </div>
   );
 }
